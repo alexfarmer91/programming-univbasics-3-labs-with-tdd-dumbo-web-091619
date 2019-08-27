a6e8a453dc1e764056035949d051214af77bd478
Alex Farmer <farmale10@gmail.com>
2019-08-27 01:20:54 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,13 @@
 # Add your variables here
 
-first_number 
+first_number = 0 
+
+second_number != 0 
+
+sum = first_number + second_number
+
+difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number 
